2defd8b10824dff93e0ad2d22637fcdfae4bce15
root <pertonsalomee@gmail.com>
2024-10-23 13:17:43 +0000

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print without char
+ * Return always 0 (Success)
+ */
+int main(void)
+{
+	int d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
