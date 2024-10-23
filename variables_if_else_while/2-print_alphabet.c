e97610be30bb995dec3d0b84bf0e79c4907195ff
root <pertonsalomee@gmail.com>
2024-10-23 11:53:50 +0000

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (Success)
+ */
+int main(void)
+{ char c = 'a';
+while (c <= 'z')
+{ putchar(c);
+c++; }
+putchar('\n');
+return (0); }
