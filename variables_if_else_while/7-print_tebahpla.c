305e0580f4051025fe36b1ceb0b98d111a4b88bb
root <pertonsalomee@gmail.com>
2024-10-23 13:39:05 +0000

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ char i = 'z';
+while (i >= 'a')
+{ putchar(i);
+i--; }
+putchar('\n');
+return (0); }
+
