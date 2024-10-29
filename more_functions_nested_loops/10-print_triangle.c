af6c5007f82abbace031e99ab7bcb51e98fa60c3
root <pertonsalomee@gmail.com>
2024-10-29 09:33:57 +0000

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,24 +6,26 @@
 
 void print_triangle(int size)
 {
+	int i, j;
+
 	if (size <= 0)
 	{
 		putchar('\n');
 	}
 	else
 	{
-		int i, j;
-
 		for (i = 1; i <= size; i++)
+		{
 			for (j = i; j < size; j++)
 			{
 				putchar(' ');
 			}
-		for (j = 1; j <= i; j++)
-		{
-			putchar('#');
+			for (j = 1; j <= i; j++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
 		}
-		putchar('\n');
 	}
 }
 
