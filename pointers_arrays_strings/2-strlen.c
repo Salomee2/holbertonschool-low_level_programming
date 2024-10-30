0a6f6db1201f1a44ead8945a0dae804bcd51536b
root <pertonsalomee@gmail.com>
2024-10-30 09:25:50 +0000

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string.
+ * @s: la chaine a mesurer
+ * Return: la longueur de la chaine
+ */
+int _strlen(char *s)
+{
+	int longueur = 0;
+
+	while (s[longueur] != '\0')
+	{
+		longueur++;
+	}
+	return longueur;
+}
