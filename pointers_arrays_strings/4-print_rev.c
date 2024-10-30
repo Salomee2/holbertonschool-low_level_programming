a3b7d4f941b715e032e3dd7777487c610362b20d
root <pertonsalomee@gmail.com>
2024-10-30 12:53:52 +0000

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -5,10 +5,19 @@
  */
 void print_rev(char *s)
 {
+	int length = 0;
+	int o;
+
 	while (*s != '\0')
 	{
+		length++;
+		s++;
+	}
+	s--;
+	for (o = length; o > 0; o--)
+	{
 		_putchar(*s);
-		++s;
+		s--;
 	}
 	_putchar('\n');
 }
