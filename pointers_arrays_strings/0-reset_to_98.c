f91a409bc1b3e81bb5f3b2541fa6afa1dee7bc3c
root <pertonsalomee@gmail.com>
2024-10-30 09:01:34 +0000

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,6 +2,7 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to to 98
+ * @n: pointeur vers l'entier dont on modifie la valeur
  */
 void reset_to_98(int *n)
 {
