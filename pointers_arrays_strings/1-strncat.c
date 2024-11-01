3fd231d6c0ecd63678efdde631d9e1f8491bf5d4
root <pertonsalomee@gmail.com>
2024-11-01 12:14:30 +0000

    1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings.
+ * @dest: first string
+ * @src: concatenated string
+ * @n: maximum number of bytes
+ * Return: *dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i;
+
+	while (dest[dest_len] != '\0')
+		dest_len++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
