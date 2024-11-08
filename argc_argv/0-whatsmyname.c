6161e56dd8ab3c4b7df43f49040cc092d3526b08
root <pertonsalomee@gmail.com>
2024-11-08 13:09:01 +0000

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Affiche le nom du programme
+ * @argc: Nombre d'arguments
+ * @argv: Tableau des arguments passés au programm
+ * Return: 0 si tout se passe bien
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
