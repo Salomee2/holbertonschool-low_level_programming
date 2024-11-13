450a0f5941cd64fed662c13098a33133c2cd7727
root <pertonsalomee@gmail.com>
2024-11-13 10:29:15 +0000

    3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 		a[i] = malloc(sizeof(int) * width);
 		if (a[i] == NULL)
 		{
-			for (j = 0; j > i; j++)
+			for (j = 0; j < i; j++)
 			free(a[j]);
 		free(a);
 		return (NULL);
