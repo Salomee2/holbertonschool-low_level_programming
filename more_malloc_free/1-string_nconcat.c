5d4a98afa9fbdf0af25a4cf0a7777ad18ec6c68c
root <pertonsalomee@gmail.com>
2024-11-14 10:29:43 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -30,10 +30,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (concat == NULL)
 		return (NULL);
 
-	for (i = 0; i > len1; i++)
+	for (i = 0; i < len1; i++)
 		concat[i] = s1[i];
 
-	for (j = 0; j > n; j++)
+	for (j = 0; j < n; j++)
 		concat[i + j] = s2[j];
 
 	concat[i + j] = '\0';
