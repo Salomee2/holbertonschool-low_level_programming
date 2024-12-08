85db0f1b8d0d98116927796db790a13a0df61d57
root <pertonsalomee@gmail.com>
2024-12-08 12:42:18 +0000

    fix3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,11 +6,11 @@
 #define BUFFER_SIZE 1024
 
 /**
- * open_file - opens a file
- * @filename: name of the file to open
- * @flags: flags for open() function
- * @mode: mode for creating the file
- * Return: file descriptor
+ * open_file - Open a file and handle errors.
+ * @filename: The name of the file to open.
+ * @flags: The flags to use when opening the file.
+ * @mode: The mode to set if the file is created.
+ * Return: The file descriptor on success.
  */
 int open_file(const char *filename, int flags, mode_t mode)
 {
@@ -27,8 +27,8 @@ return (fd);
 }
 
 /**
- * close_file - closes a file descriptor
- * @fd: file descriptor to close
+ * close_file - Close a file descriptor and handle errors.
+ * @fd: The file descriptor to close.
  */
 void close_file(int fd)
 {
@@ -40,29 +40,29 @@ exit(100);
 }
 
 /**
- * main - copies the content of a file to another file
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: 0 on success
+ * main - Copies the content of a file to another file.
+ * @argc: Number of arguments passed to the program.
+ * @argv: Arguments passed to the program.
+ * Return: 0 on success, or exit with a corresponding code on error.
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 int fd_from, fd_to, bytes_read, bytes_written;
 char buffer[BUFFER_SIZE];
 
 if (argc != 3)
 {
-dprintf(2, "Usage: cp file_from file_to\n");
+dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
 exit(97);
 }
 
 fd_from = open_file(argv[1], O_RDONLY, 0);
-fd_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+fd_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
 while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 {
 bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written != bytes_read)
+if (bytes_written == -1)
 {
 dprintf(2, "Error: Can't write to %s\n", argv[2]);
 exit(99);
@@ -80,6 +80,4 @@ close_file(fd_to);
 
 return (0);
 }
-
-
 
